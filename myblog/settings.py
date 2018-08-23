3f468e591290b3576c8c7e3895df5c0e30351107
gbrowsilva <gsilvabrow@gmail.com>
2018-08-23 19:59:22 -0300

    Alterando nome incorreto

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'stb=m!!$)rg)0-d52ldobwnhbrl+#nt0&l7!@yn%zb8pwbe!y_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonaywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
