84478891996ea3987f7e43b20040f7ff400ec9a4
Gilmar Silva <gsilvabrow@gmail.com>
2018-08-30 20:18:05 -0300

    adicionado o templete post_list.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
@@ -7,3 +8,7 @@ def post_list(request):
     posts = Post.objects.filter(publisher_date__lte=timezone.now())\
         .order_by('publisher_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
